b1a837c3de848e3ae3329455b24d8c06343eac0a
Dmitry <deekovalev@gmail.com>
2022-03-16 23:56:37 +0300

    wayfire updates

--- .config/wob_start.sh
+++ .config/wob_start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 WOBSOCK=${WOBSOCK:-"$XDG_RUNTIME_DIR/wob.sock"}
+[ -r "$WOBSOCK" ] && rm "$WOBSOCK"
 mkfifo "$WOBSOCK" || exit 1
 trap 'rm "$WOBSOCK"' EXIT
 exec tail --follow=name "$WOBSOCK" | wob \
