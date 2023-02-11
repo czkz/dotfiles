41501eadb7aabe9723be58ea239dd839143cd46d
Dmitry <deekovalev@gmail.com>
2023-02-12 00:13:59 +0400

    wayland updates

--- .config/wob_start.sh
+++ .config/wob_start.sh
@@ -3,14 +3,4 @@ WOBSOCK=${WOBSOCK:-"$XDG_RUNTIME_DIR/wob.sock"}
 [ -r "$WOBSOCK" ] && rm "$WOBSOCK"
 mkfifo "$WOBSOCK" || exit 1
 trap 'rm "$WOBSOCK"' EXIT
-exec tail -f "$WOBSOCK" | wob \
-    --anchor bottom \
-    --width 600 \
-    --height 30 \
-    --offset 0 \
-    --border 1 \
-    --padding 2 \
-    --margin 200 \
-    --border-color \#0496FFFF \
-    --background-color \#000000EE \
-    --bar-color \#C5C8C6FF
+exec tail -f "$WOBSOCK" | wob
